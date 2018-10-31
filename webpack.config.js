bd24a633ccc7c148a064b99c1322835194664943
user <user@debian.debian>
2018-10-31 19:17:45 +0300

    init proj

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const mode = process.env.NODE_ENV ? process.env.NODE_ENV: 'development';
+
+module.exports = {
+  mode: mode,
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  entry: {
+    main: './src/index.ts',
+  },
+  output: {
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: {
+      rewrites: [
+        { from: '/', to: '/heroes.html' },
+      ]
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'heroes.html',
+    }),
+    new CleanWebpackPlugin(['dist'], {
+      verbose: false,
+    }),
+    new CheckerPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+        cacheGroups: {
+          vendor: {
+                test: /node_modules/,
+                name: "vendor",
+                chunks: "initial",
+                enforce: true
+            }
+        }
+    }
+  }
+};
