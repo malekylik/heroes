0a61435d07568ef99b6b011190e858263e685fb7
Maxim Kalinovskiy <malekylik@gmail.com>
2018-12-24 22:44:20 +0300

    ray-tracing: implimented CanvasGl

---
+++ src/render/canvas/canvas-gl.ts
@@ -0,0 +1,45 @@
+import * as glm from 'glm-js'
+
+import { Canvas } from './canvas';
+
+export class CanvasGl extends Canvas {
+    private readonly _gl: WebGLRenderingContext;
+
+    private static canvas: CanvasGl;
+
+    get gl(): WebGLRenderingContext {
+        return this._gl;
+    }
+
+    clear(): void {
+        this._gl.clear(this._gl.COLOR_BUFFER_BIT | this._gl.DEPTH_BUFFER_BIT);
+    }
+
+    enableDepthTest(): void {
+        this._gl.enable(this._gl.DEPTH_TEST);
+    }
+
+    set bgColor(rgba: glm.vec4) {
+        super.bgColor = rgba;
+        this._gl.clearColor(rgba.r, rgba.g, rgba.b, rgba.a);
+    }
+
+    setSize(width: number, height: number): void {
+        super.setSize(width, height);
+        this._gl.viewport(0, 0, this._gl.drawingBufferWidth, this._gl.drawingBufferHeight);
+    }
+
+    static getCanvas(): CanvasGl {
+        if (!CanvasGl.canvas) {
+            const canvasHTML: HTMLCanvasElement = document.createElement('canvas');
+            CanvasGl.canvas = new CanvasGl(canvasHTML);
+        }
+
+        return CanvasGl.canvas;
+    }
+
+    protected constructor(canvasHTML: HTMLCanvasElement) {
+        super(canvasHTML);
+        this._gl = canvasHTML.getContext('webgl');
+    }
+}
--- src/render/canvas/canvas.ts
+++ src/render/canvas/canvas.ts
@@ -1,58 +1,32 @@
 import * as glm from 'glm-js'
 
-export class Canvas {
+export abstract class Canvas {
 
   private readonly _canvasHTML: HTMLCanvasElement;
-  private readonly _gl: WebGLRenderingContext;
   private _bgColor: glm.vec4 = glm.vec4(0.0, 0.0, 0.0, 1.0);
-
-  private static canvas: Canvas;
 
   get bgColor(): glm.vec4 {
     return this._bgColor;
   }
 
-  get canvasHTML(): HTMLCanvasElement {
-    return this._canvasHTML;
-  }
-
-  get gl(): WebGLRenderingContext {
-    return this._gl;
-  }
-
-  clear(): void {
-    this._gl.clear(this._gl.COLOR_BUFFER_BIT | this._gl.DEPTH_BUFFER_BIT);
-  }
-
-  enableDepthTest(): void {
-    this._gl.enable(this._gl.DEPTH_TEST);
-  }
-
-  setColor(rgba: glm.vec4): void {
-    this._gl.clearColor(rgba.r, rgba.g, rgba.b, rgba.a);
+  set bgColor(rgba: glm.vec4) {
     this._bgColor = rgba;
   }
 
+  get canvasHTML(): HTMLCanvasElement {
+    return this._canvasHTML;
+  }
+  
   setSize(width: number, height: number): void {
     this._canvasHTML.style.width = `${width}px`;
     this._canvasHTML.style.height = `${height}px`;
-    this._gl.canvas.width = width;
-    this._gl.canvas.height = height;
-    this._gl.viewport(0, 0, this._gl.drawingBufferWidth, this._gl.drawingBufferHeight);
+    this._canvasHTML.width = width;
+    this._canvasHTML.height = height;
   }
 
-  static getCanvas(): Canvas {
-    if (!Canvas.canvas) {
-      const canvasHTML: HTMLCanvasElement = document.createElement('canvas');
-      Canvas.canvas = new Canvas(canvasHTML);
-    }
+  abstract clear(): void;
 
-    return Canvas.canvas;
-  }
-
-  private constructor(canvasHTML: HTMLCanvasElement) {
+  constructor(canvasHTML: HTMLCanvasElement) {
     this._canvasHTML = canvasHTML;
-    this._gl = canvasHTML.getContext('webgl');
   }
-
 }
