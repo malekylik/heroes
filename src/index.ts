6d4af5ceb24540dc570047e0963c819de6363328
Maxim Kalinovskiy <malekylik@gmail.com>
2018-12-24 23:04:41 +0300

    ray-tracing: implimented Canvas2D

--- src/index.ts
+++ src/index.ts
@@ -1,139 +1,149 @@
 import * as glm from 'glm-js';
-import * as OBJ from 'webgl-obj-loader';
+// import * as OBJ from 'webgl-obj-loader';
 
-// import { vec3 } from './linear-math';
+import { vec3 } from './linear-math';
+import { Canvas2D } from './render/canvas/canvas2d';
 
-// class Sphere {
-//   constructor(public center: vec3, private radius: number, public color: vec3) { }
+class Sphere {
+  constructor(public center: vec3, private radius: number, public color: vec3) { }
 
-//   intersectRay(cameraPos: vec3, pixelPos: vec3): { t1: number, t2: number } {
-//     const oc: vec3 = cameraPos.sub(this.center);
+  intersectRay(cameraPos: vec3, pixelPos: vec3): { t1: number, t2: number } {
+    const oc: vec3 = cameraPos.sub(this.center);
 
-//     const k1: number = pixelPos.dot(pixelPos);
-//     const k2: number = oc.dot(pixelPos) * 2;
-//     const k3: number = oc.dot(oc) - this.radius * this.radius;
+    const k1: number = pixelPos.dot(pixelPos);
+    const k2: number = oc.dot(pixelPos) * 2;
+    const k3: number = oc.dot(oc) - this.radius * this.radius;
 
-//     const discriminant: number = k2 * k2 - 4 * k1 * k3;
+    const discriminant: number = k2 * k2 - 4 * k1 * k3;
 
-//     if (discriminant < 0) {
-//       return {
-//         t1: Number.MAX_SAFE_INTEGER,
-//         t2: Number.MAX_SAFE_INTEGER,
-//       };
-//     }
+    if (discriminant < 0) {
+      return {
+        t1: Number.MAX_SAFE_INTEGER,
+        t2: Number.MAX_SAFE_INTEGER,
+      };
+    }
 
-//     return {
-//       t1: (-k2 + Math.sqrt(discriminant)) / (2 * k1),
-//       t2: (-k2 - Math.sqrt(discriminant)) / (2 * k1),
-//     };
-//   }
-// }
+    return {
+      t1: (-k2 + Math.sqrt(discriminant)) / (2 * k1),
+      t2: (-k2 - Math.sqrt(discriminant)) / (2 * k1),
+    };
+  }
+}
 
-// abstract class Light {
-//   constructor(public intensity: number) { }
+abstract class Light {
+  constructor(public intensity: number) { }
 
-//   abstract computeLight(position: vec3, normal: vec3): number;
-// }
+  abstract computeLight(position: vec3, normal: vec3): number;
+}
 
-//  class AmbientLight extends Light {
-//   computeLight(_: vec3, __: vec3): number {
-//     return this.intensity;
-//   }
-// }
+ class AmbientLight extends Light {
+  computeLight(_: vec3, __: vec3): number {
+    return this.intensity;
+  }
+}
 
-// class PointLight extends Light {
-//   constructor(intensity: number, public position: vec3) {
-//     super(intensity);
-//   }
+class PointLight extends Light {
+  constructor(intensity: number, public position: vec3) {
+    super(intensity);
+  }
   
   
-//   computeLight(position: vec3, normal: vec3): number {
-//     const L: vec3 = this.position.sub(position);
-//     const nDotL = normal.dot(L);
-//     let i: number = 0;
+  computeLight(position: vec3, normal: vec3): number {
+    const L: vec3 = this.position.sub(position);
+    const nDotL = normal.dot(L);
+    let i: number = 0;
 
-//     if (nDotL > 0) {
-//       i += this.intensity * nDotL / (normal.length() * L.length());
-//     }
+    if (nDotL > 0) {
+      i += this.intensity * nDotL / (normal.length() * L.length());
+    }
 
-//     return i;
-//   }
-// }
+    return i;
+  }
+}
 
-// class DirectionalLight extends Light {
-//   constructor(intensity: number, public direction: vec3) {
-//     super(intensity);
-//   }
+class DirectionalLight extends Light {
+  constructor(intensity: number, public direction: vec3) {
+    super(intensity);
+  }
 
-//   computeLight(_: vec3, normal: vec3): number {
-//     const L: vec3 = this.direction;
-//     const nDotL = normal.dot(L);
-//     let i: number = 0;
+  computeLight(_: vec3, normal: vec3): number {
+    const L: vec3 = this.direction;
+    const nDotL = normal.dot(L);
+    let i: number = 0;
 
-//     if (nDotL > 0) {
-//       i += this.intensity * nDotL / (normal.length() * L.length());
-//     }
+    if (nDotL > 0) {
+      i += this.intensity * nDotL / (normal.length() * L.length());
+    }
 
-//     return i;
-//   }
-// }
+    return i;
+  }
+}
 
-// function putPixel(canvas: HTMLCanvasElement, canvasBuffer: ImageData, canvasPitch: number, x: number, y: number, color: vec3): void {
-//   x = canvas.width / 2 + x;
-//   y = canvas.height / 2 - y - 1;
+function putPixel(canvas: HTMLCanvasElement, canvasBuffer: ImageData, canvasPitch: number, x: number, y: number, color: vec3): void {
+  x = canvas.width / 2 + x;
+  y = canvas.height / 2 - y - 1;
 
-//   if (x < 0 || x >= canvas.width || y < 0 || y >= canvas.height) {
-//     return;
-//   }
+  if (x < 0 || x >= canvas.width || y < 0 || y >= canvas.height) {
+    return;
+  }
 
-//   let offset = 4 * x + canvasPitch * y;
-//   canvasBuffer.data[offset++] = color.x;
-//   canvasBuffer.data[offset++] = color.y;
-//   canvasBuffer.data[offset++] = color.z;
-//   canvasBuffer.data[offset++] = 255;
-// }
+  let offset = 4 * x + canvasPitch * y;
+  canvasBuffer.data[offset++] = color.x;
+  canvasBuffer.data[offset++] = color.y;
+  canvasBuffer.data[offset++] = color.z;
+  canvasBuffer.data[offset++] = 255;
+}
 
-// function canvasToViewport(x: number, y: number, vw: number, vh: number, cw: number, ch: number, d: number): vec3 {
-//   return vec3(x * vw / cw, y * vh / ch, d);
-// }
+function canvasToViewport(x: number, y: number, vw: number, vh: number, cw: number, ch: number, d: number): vec3 {
+  return vec3(x * vw / cw, y * vh / ch, d);
+}
 
-// function clamp(v: vec3): vec3 {
-//   return vec3(Math.min(255, Math.max(0, v.x)), Math.min(255, Math.max(0, v.y)), Math.min(255, Math.max(0, v.z)));
-// }
+function clamp(v: vec3): vec3 {
+  return vec3(Math.min(255, Math.max(0, v.x)), Math.min(255, Math.max(0, v.y)), Math.min(255, Math.max(0, v.z)));
+}
 
-// function traceRay(cameraPos: vec3, pixelPos: vec3, scene: Array<Sphere>, ligths: Array<Light>, tMin: number, tMax: number): vec3 {
-//   let closestSphere: Sphere = null;
-//   let closestT: number = Number.MAX_SAFE_INTEGER;
+function traceRay(cameraPos: vec3, pixelPos: vec3, scene: Array<Sphere>, ligths: Array<Light>, tMin: number, tMax: number): vec3 {
+  let closestSphere: Sphere = null;
+  let closestT: number = Number.MAX_SAFE_INTEGER;
 
-//   for (let sphere of scene) {
-//     const { t1, t2 }: { t1: number, t2: number } = sphere.intersectRay(cameraPos, pixelPos);
+  for (let sphere of scene) {
+    const { t1, t2 }: { t1: number, t2: number } = sphere.intersectRay(cameraPos, pixelPos);
 
-//     if ((t1 >= tMin && t1 <= tMax) && t1 < closestT) {
-//       closestT = t1;
-//       closestSphere = sphere;
-//     }
+    if ((t1 >= tMin && t1 <= tMax) && t1 < closestT) {
+      closestT = t1;
+      closestSphere = sphere;
+    }
 
-//     if ((t2 >= tMin && t2 <= tMax) && t2 < closestT) {
-//       closestT = t2;
-//       closestSphere = sphere;
-//     }
-//   }
+    if ((t2 >= tMin && t2 <= tMax) && t2 < closestT) {
+      closestT = t2;
+      closestSphere = sphere;
+    }
+  }
 
-//   if (closestSphere === null) {
-//     return vec3(0, 0, 0);
-//   }
+  if (closestSphere === null) {
+    return vec3(0, 0, 0);
+  }
 
-//   const P: vec3 = cameraPos.add(pixelPos.mul(closestT));
-//   let N: vec3 = P.sub(closestSphere.center);
-//   N = N.mul(1 / N.length());
-//   let i: number = 0;
+  const P: vec3 = cameraPos.add(pixelPos.mul(closestT));
+  let N: vec3 = P.sub(closestSphere.center);
+  N = N.mul(1 / N.length());
+  let i: number = 0;
 
-//   for (let ligth of ligths) {
-//     i += ligth.computeLight(P, N);
-//   }
+  for (let ligth of ligths) {
+    i += ligth.computeLight(P, N);
+  }
 
-//   return closestSphere.color.mul(i);
-// }
+  return closestSphere.color.mul(i);
+}
+
+const canvas: Canvas2D = Canvas2D.getCanvas();
+
+document.body.appendChild(canvas.canvasHTML);
+
+canvas.setSize(400, 400);
+canvas.bgColor = glm.vec4(100, 50, 20, 1);
+canvas.clear();
+canvas.render();
 
 // const canvasHTML: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
 // const ctx: CanvasRenderingContext2D = canvasHTML.getContext('2d');
@@ -178,122 +188,122 @@ import * as OBJ from 'webgl-obj-loader';
 
 // console.log(`time: ${Date.now() - now}`);
 
-import { CanvasGl } from './render/canvas/canvas-gl';
+// import { CanvasGl } from './render/canvas/canvas-gl';
 
-const canvas: CanvasGl = CanvasGl.getCanvas();
-const gl: WebGLRenderingContext = canvas.gl;
-const width: number = window.innerWidth;
-const height: number = window.innerHeight;
-const bgColor: glm.vec4 = glm.vec4(0.0, 0.5, 0.0, 1.0);
-const perspective: glm.mat4 = glm.perspective(glm.radians(90), width / height, 1, 1000);
-const translate: glm.mat4 = glm.translate(glm.vec3(-200, -200, -200));
-const path: string = './src/IronMan.obj';
-const A_POSITION: string = 'a_Position';
-const PERSPECTIVE: string = 'perspective';
-const TRANSLATE: string = 'translate';
-const ROTATION: string = 'rotation';        
-let rotation: glm.mat4 = glm.toMat4(glm.angleAxis(glm.radians(45), glm.vec3(0, 1, 1)));
-let verticesCoord: Float32Array;
-let obj: OBJ.Mesh;
+// const canvas: CanvasGl = CanvasGl.getCanvas();
+// const gl: WebGLRenderingContext = canvas.gl;
+// const width: number = window.innerWidth;
+// const height: number = window.innerHeight;
+// const bgColor: glm.vec4 = glm.vec4(0.0, 0.5, 0.0, 1.0);
+// const perspective: glm.mat4 = glm.perspective(glm.radians(90), width / height, 1, 1000);
+// const translate: glm.mat4 = glm.translate(glm.vec3(-200, -200, -200));
+// const path: string = './src/IronMan.obj';
+// const A_POSITION: string = 'a_Position';
+// const PERSPECTIVE: string = 'perspective';
+// const TRANSLATE: string = 'translate';
+// const ROTATION: string = 'rotation';        
+// let rotation: glm.mat4 = glm.toMat4(glm.angleAxis(glm.radians(45), glm.vec3(0, 1, 1)));
+// let verticesCoord: Float32Array;
+// let obj: OBJ.Mesh;
 
-const VSHADER_SOURCE: string =
-  `uniform mat4 ${TRANSLATE}; \n
-  uniform mat4 ${ROTATION}; \n
-  uniform mat4 ${PERSPECTIVE}; \n
-  attribute vec4 ${A_POSITION};\n
-  void main() {\n
-   gl_Position = ${PERSPECTIVE} * ${TRANSLATE} * ${ROTATION} * ${A_POSITION};\n
-  }\n`;
-const FSHADER_SOURCE: string =
-  `void main() {\n
-   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n
-  }\n`;
+// const VSHADER_SOURCE: string =
+//   `uniform mat4 ${TRANSLATE}; \n
+//   uniform mat4 ${ROTATION}; \n
+//   uniform mat4 ${PERSPECTIVE}; \n
+//   attribute vec4 ${A_POSITION};\n
+//   void main() {\n
+//    gl_Position = ${PERSPECTIVE} * ${TRANSLATE} * ${ROTATION} * ${A_POSITION};\n
+//   }\n`;
+// const FSHADER_SOURCE: string =
+//   `void main() {\n
+//    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n
+//   }\n`;
 
-const program: WebGLProgram = createProgram(VSHADER_SOURCE, FSHADER_SOURCE);
-gl.useProgram(program);
+// const program: WebGLProgram = createProgram(VSHADER_SOURCE, FSHADER_SOURCE);
+// gl.useProgram(program);
 
-const a_Position: number = gl.getAttribLocation(program, A_POSITION);
-const perspectiveUniformLocation: WebGLUniformLocation =
-  gl.getUniformLocation(program, PERSPECTIVE);
-const translateUniformLocation: WebGLUniformLocation = gl.getUniformLocation(program, TRANSLATE);
-const rotationUniformLocation: WebGLUniformLocation = gl.getUniformLocation(program, ROTATION);
+// const a_Position: number = gl.getAttribLocation(program, A_POSITION);
+// const perspectiveUniformLocation: WebGLUniformLocation =
+//   gl.getUniformLocation(program, PERSPECTIVE);
+// const translateUniformLocation: WebGLUniformLocation = gl.getUniformLocation(program, TRANSLATE);
+// const rotationUniformLocation: WebGLUniformLocation = gl.getUniformLocation(program, ROTATION);
 
-function loadShader(type: number, source: string): WebGLShader {
-  const shader: WebGLShader = gl.createShader(type);
+// function loadShader(type: number, source: string): WebGLShader {
+//   const shader: WebGLShader = gl.createShader(type);
 
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
+//   gl.shaderSource(shader, source);
+//   gl.compileShader(shader);
 
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    const error: string = gl.getShaderInfoLog(shader);
-    throw new Error(`invalid compile status shader: ${error}`);
-  }
+//   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+//     const error: string = gl.getShaderInfoLog(shader);
+//     throw new Error(`invalid compile status shader: ${error}`);
+//   }
 
-  return shader;
-}
+//   return shader;
+// }
 
-function createProgram(vShader: string, fShader: string): WebGLProgram {
-  const vertexShader: WebGLShader = loadShader(gl.VERTEX_SHADER, vShader);
-  const fragmentShader: WebGLShader = loadShader(gl.FRAGMENT_SHADER, fShader);
-  const program: WebGLProgram = gl.createProgram();
+// function createProgram(vShader: string, fShader: string): WebGLProgram {
+//   const vertexShader: WebGLShader = loadShader(gl.VERTEX_SHADER, vShader);
+//   const fragmentShader: WebGLShader = loadShader(gl.FRAGMENT_SHADER, fShader);
+//   const program: WebGLProgram = gl.createProgram();
 
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
+//   gl.attachShader(program, vertexShader);
+//   gl.attachShader(program, fragmentShader);
+//   gl.linkProgram(program);
 
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    const error: string = gl.getProgramInfoLog(program); 
-    throw new Error(`not the last link operation was successful: ${error}`);
-  }
+//   if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+//     const error: string = gl.getProgramInfoLog(program); 
+//     throw new Error(`not the last link operation was successful: ${error}`);
+//   }
 
-  return program;
-}
+//   return program;
+// }
 
-function render(time: number): void {
-  const vertexBuffer: WebGLBuffer = gl.createBuffer();
-  const indicesBuffer: WebGLBuffer = gl.createBuffer();
+// function render(time: number): void {
+//   const vertexBuffer: WebGLBuffer = gl.createBuffer();
+//   const indicesBuffer: WebGLBuffer = gl.createBuffer();
 
-  rotation = glm.toMat4(glm.angleAxis(glm.radians(time / 10), glm.vec3(0, 1, 0)));
-  gl.uniformMatrix4fv(rotationUniformLocation, false, rotation.elements);
+//   rotation = glm.toMat4(glm.angleAxis(glm.radians(time / 10), glm.vec3(0, 1, 0)));
+//   gl.uniformMatrix4fv(rotationUniformLocation, false, rotation.elements);
 
-  verticesCoord = new Float32Array(obj.vertices);
+//   verticesCoord = new Float32Array(obj.vertices);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, verticesCoord, gl.STATIC_DRAW);
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(obj.indices), gl.STATIC_DRAW);
-  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_Position);
+//   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+//   gl.bufferData(gl.ARRAY_BUFFER, verticesCoord, gl.STATIC_DRAW);
+//   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+//   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(obj.indices), gl.STATIC_DRAW);
+//   gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+//   gl.enableVertexAttribArray(a_Position);
 
-  canvas.clear();
-  gl.drawElements(gl.TRIANGLES, obj.indices.length, gl.UNSIGNED_SHORT, 0);
+//   canvas.clear();
+//   gl.drawElements(gl.TRIANGLES, obj.indices.length, gl.UNSIGNED_SHORT, 0);
 
-  gl.deleteBuffer(vertexBuffer);
-  gl.deleteBuffer(indicesBuffer);
-}
+//   gl.deleteBuffer(vertexBuffer);
+//   gl.deleteBuffer(indicesBuffer);
+// }
 
-function main(time: number) {
-  render(time);
-  requestAnimationFrame(main);
-}
+// function main(time: number) {
+//   render(time);
+//   requestAnimationFrame(main);
+// }
 
-async function start(): Promise<void> {
-  const response: Response = await fetch(path);
-  const stringObj: string = await response.text();
+// async function start(): Promise<void> {
+//   const response: Response = await fetch(path);
+//   const stringObj: string = await response.text();
 
-  obj = new OBJ.Mesh(stringObj);
+//   obj = new OBJ.Mesh(stringObj);
 
-  requestAnimationFrame(main);
-}
+//   requestAnimationFrame(main);
+// }
 
-document.body.appendChild(canvas.canvasHTML);
+// document.body.appendChild(canvas.canvasHTML);
 
-gl.uniformMatrix4fv(perspectiveUniformLocation, false, perspective.elements);
-gl.uniformMatrix4fv(translateUniformLocation, false, translate.elements);
-gl.uniformMatrix4fv(rotationUniformLocation, false, rotation.elements);
+// gl.uniformMatrix4fv(perspectiveUniformLocation, false, perspective.elements);
+// gl.uniformMatrix4fv(translateUniformLocation, false, translate.elements);
+// gl.uniformMatrix4fv(rotationUniformLocation, false, rotation.elements);
 
-canvas.bgColor = bgColor;
-canvas.enableDepthTest();
-canvas.setSize(width, height);
+// canvas.bgColor = bgColor;
+// canvas.enableDepthTest();
+// canvas.setSize(width, height);
 
-start();
+// start();
---
+++ src/render/canvas/canvas2d.ts
@@ -0,0 +1,62 @@
+import { Canvas } from './canvas';
+import { vec3 } from '../../linear-math';
+import * as glm from 'glm-js'
+
+export class Canvas2D extends Canvas {
+    private readonly ctx: CanvasRenderingContext2D;
+
+    private static canvas: Canvas2D;
+    private buffer: ImageData;
+    private canvasPitch: number;
+
+    get context(): CanvasRenderingContext2D {
+        return this.ctx;
+    }
+
+    clear(): void {
+        for (let x = 0; x < this.canvasHTML.width; x++) {
+            for (let y = 0; y < this.canvasHTML.height; y++) {
+                this.putPixel(x, y, this.bgColor);
+            }
+        }
+    }
+
+    putPixel(x: number, y: number, color: glm.vec4): void {
+        if (x < 0 || x >= this.canvasHTML.width || y < 0 || y >= this.canvasHTML.height) {
+            throw new Error('out of range');
+          }
+
+          let offset = 4 * x + this.canvasPitch * y;
+          this.buffer.data[offset++] = color.x;
+          this.buffer.data[offset++] = color.y;
+          this.buffer.data[offset++] = color.z;
+          this.buffer.data[offset++] = 255;
+    }
+
+    render(): void {
+        this.ctx.putImageData(this.buffer, 0, 0);
+    }
+
+    setSize(width: number, height: number): void {
+        super.setSize(width, height);
+        this.buffer = this.ctx.getImageData(0, 0, this.canvasHTML.width, this.canvasHTML.height);
+        this.canvasPitch = this.buffer.width * 4;
+    }
+
+    static getCanvas(): Canvas2D {
+        if (!Canvas2D.canvas) {
+            const canvasHTML: HTMLCanvasElement = document.createElement('canvas');
+            Canvas2D.canvas = new Canvas2D(canvasHTML);
+        }
+
+        return Canvas2D.canvas;
+    }
+
+    protected constructor(canvasHTML: HTMLCanvasElement) {
+        super(canvasHTML);
+        this.ctx = canvasHTML.getContext('2d');
+
+        this.buffer = this.ctx.getImageData(0, 0, this.canvasHTML.width, this.canvasHTML.height);
+        this.canvasPitch = this.buffer.width * 4;
+    }
+}
