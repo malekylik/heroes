90ea124e5097ce855aa09057d37cdf7110b77644
Maxim Kalinovskiy <malekylik@gmail.com>
2019-03-30 18:53:50 +0300

    memory-allocation/feat: update Int type name to Int32

--- src/memory/types/Int/Int32.ts
+++ src/memory/types/Int/Int32.ts
@@ -3,8 +3,12 @@ import { Allocator, get4Byte, set4Byte } from '../../allocator';
 import { toInt32 } from '../../coercion';
 
 export type Int32P = Pointer;
-export const INT_SIZE: number = 4;
-export const Int: Primitive = new Primitive(INT_SIZE);
+export const INT32_SIZE: number = 4;
+export const Int32: Primitive = new Primitive(INT32_SIZE);
+
+Int32.toString = function toString(): string {
+    return 'Int32';
+}
 
 export function getInt32(a: Allocator, address: Int32P): number {
     return get4Byte(a, address);
