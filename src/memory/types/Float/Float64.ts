e92b8032b9408f6ea21747c0637da8d3a3fe6325
Maxim Kalinovskiy <malekylik@gmail.com>
2019-04-02 22:06:05 +0300

    memory-allocation/feat: add Float64 type

---
+++ src/memory/types/Float/Float64.ts
@@ -0,0 +1,18 @@
+import { Pointer, Primitive } from '..';
+import { Allocator, get8Byte, set8Byte } from '../../allocator';
+
+export type Float64P = Pointer;
+export const FLOAT64_SIZE: number = 8;
+export const Float64: Primitive = new Primitive(FLOAT64_SIZE);
+
+Float64.toString = function toString(): string {
+    return 'Float64';
+}
+
+export function getFloat64(a: Allocator, address: Float64P): number {
+    return get8Byte(a, address);
+}
+
+export function setFloat64(a: Allocator, address: Float64P, v: number): number {
+    return set8Byte(a, address, v);
+}
